32cbaf28a520f8963ca15c0aef56fc39409e0b49
guillenec <guillermoneculqueo@gmail.com>
2025-08-01 00:53:50 -0300

    fix sppiner color Blest

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,9 +2,11 @@
 
 const Loading = () => {
   return (
-    <div className="flex-col gap-4 w-full flex items-center justify-center">
-      <div className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
-        <div className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"></div>
+    <div className="flex-col gap-2 w-full flex items-center justify-center">
+      <div className="w-20 h-20 border-4 border-transparent animate-spin  flex items-center justify-center border-t-[#b1182e] rounded-full">
+        <div className="w-16 h-16 border-4 border-transparent animate-spin duration-3000 flex items-center justify-center border-t-[#283732] rounded-full">
+          <div className="w-12 h-12 border-4 border-transparent animate-spin duration-5000 flex items-center justify-center border-t-[#eee0a1] rounded-full" />
+        </div>
       </div>
     </div>
 
--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,7 +1,12 @@
+import Loading from "../../components/Loading/Loading"
+
 const Cart = () => {
   return (
   <main className='flex flex-col items-center justify-center h-full'>
     <h1 className='text-4xl font-bold text-[#b1182e]'>Cart</h1>
+    <section className="w-full h-72 flex justify-center items-center">
+      <Loading />
+    </section>
   </main>  )
 }
 
